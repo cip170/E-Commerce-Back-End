6c6b3e437d630b794b4ff987b4ce77f1ad6d8a22
cip170 <charlottephilipps@gmail.com>
2023-09-11 20:28:00 +1000

    adding correct route files & index.js belongsTo

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+});
 // Categories have many Products
+Product.hasMany(Product, {
 
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+});
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Product, {
+
+});
 
 module.exports = {
   Product,
